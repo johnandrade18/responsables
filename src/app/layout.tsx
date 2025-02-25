f86a5adc5971aedf87da1ae11ae6bd46c7f98c21
John Andrade <sistemas@johnandrade.com>
2025-02-25 09:06:03 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-8">
           {children}
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
